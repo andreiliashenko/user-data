d7e8e029877236ba764f0f5c958e23200065e872
andreiliashenko <lyashenkoav92@gmail.com>
2015-08-15 23:26:37 +0400

    Id range constants are added to user grant interface

--- src/main/java/com/anli/generalization/user/data/entities/UserGrant.java
+++ src/main/java/com/anli/generalization/user/data/entities/UserGrant.java
@@ -4,6 +4,9 @@ import java.math.BigInteger;
 
 public interface UserGrant {
 
+    BigInteger MIN_GRANT_ID = BigInteger.valueOf(2000000000000000000L);
+    BigInteger MAX_GRANT_ID = BigInteger.valueOf(2000000000000999999L);
+
     BigInteger getId();
 
     String getDescription();
